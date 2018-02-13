ce875616d2758ceebd1c1215be73dceca62bfe80
AlekseiTepljakov <alex@starspirals.net>
2018-02-13 23:41:22 +0200

    [Feature] Tuning update: initial commit (to be tested); TODO: make notes update after tuning changed (currently this is not working, idea: just find enumerate all highlighted notes and restore later)

--- js/composer-tools.js
+++ js/composer-tools.js
@@ -1302,191 +1302,213 @@ function comptoolsFretboard(cont_class, tuning, options) {
     }
 
     this.fret_notes = new Object();
-    this.fret_notes.note = Array();
-    this.fret_notes.index = Array();
-    this.fret_notes.full_note = Array();
-    this.fret_notes.count = null;
+    this.notes = null;
+    this.tuning = tuning.toUpperCase();
+    this.cont_class = cont_class;
+    this.svg_fretboard = null;
 
-    // Create note/index arrays
-    var fret_current_tuning = tuning;
-    fret_current_tuning.toUpperCase();
+    this.updateTuning = function (tuning) {
+        
+        // Check optional argument
+        if (typeof tuning !== 'undefined' && tuning !== null && tuning !== "")
+        {
+            this.tuning = tuning.toUpperCase(); // Update tuning property
+        }
+        
+        var cont_class = this.cont_class;
+        var fret_current_tuning = this.tuning;
+        
+        // Remove the SVG (if it exists)
+        d3.select(cont_class).selectAll('*').remove()
 
-    match = note_scinot_global.exec(fret_current_tuning);
-    while (match !== null) {
-        this.fret_notes.full_note.push(match[0]);
-        this.fret_notes.note.push(match[1]);
-        this.fret_notes.index.push(match[2]);
-        this.fret_notes.count++;
+        // Create note/index arrays
+        this.fret_notes.note = Array();
+        this.fret_notes.index = Array();
+        this.fret_notes.full_note = Array();
+        this.fret_notes.count = null;
+
         match = note_scinot_global.exec(fret_current_tuning);
-    }
+        while (match !== null) {
+            this.fret_notes.full_note.push(match[0]);
+            this.fret_notes.note.push(match[1]);
+            this.fret_notes.index.push(match[2]);
+            this.fret_notes.count++;
+            match = note_scinot_global.exec(fret_current_tuning);
+        }
 
-    // Get the container height and width
-    // NB! These should always be set!
-    var fret_conw = parseFloat(d3.select(cont_class).style("width"));
-    var fret_conh = parseFloat(d3.select(cont_class).style("height"));
+        // Get the container height and width
+        // NB! These should always be set!
+        var fret_conw = parseFloat(d3.select(cont_class).style("width"));
+        var fret_conh = parseFloat(d3.select(cont_class).style("height"));
 
-    var fret_w = fret_conw - FRET_X_OFFSET;
-    var fret_h = fret_conh - FRET_Y_OFFSET;
+        var fret_w = fret_conw - FRET_X_OFFSET;
+        var fret_h = fret_conh - FRET_Y_OFFSET;
 
-    // Create the SVG container: fills the containing container
-    this.svg_fretboard = d3.select(cont_class).append("svg")
-            .attr("width", fret_w)
-            .attr("height", fret_h);
+        // Create the SVG container: fills the containing container
+        this.svg_fretboard = d3.select(cont_class).append("svg")
+                .attr("width", fret_w)
+                .attr("height", fret_h);
 
-    // Single string height
-    var fret_string_h = Math.floor(fret_h / (this.fret_notes.count + 1));
+        // Single string height
+        var fret_string_h = Math.floor(fret_h / (this.fret_notes.count + 1));
 
-    var fret_x_coord = FRET_X_OFFSET;
+        var fret_x_coord = FRET_X_OFFSET;
 
-    // We will need to find the smallest fret width to determine the
-    // size of the note markers. Also, we need all center positions.
-    var centerPos = Array();
-    var fret_min_width = firstFretWidth;
+        // We will need to find the smallest fret width to determine the
+        // size of the note markers. Also, we need all center positions.
+        var centerPos = Array();
+        var fret_min_width = firstFretWidth;
 
-    // We start with the open position (a third of the first fret size)
-    var now_fret_size = Math.floor(firstFretWidth / 3);
-    fret_min_width = Math.min(fret_min_width, now_fret_size);
-    this.svg_fretboard.append("rect").attr("x", fret_x_coord)
-            .attr("y", FRET_Y_OFFSET)
-            .attr("width", now_fret_size)
-            .attr("height", fret_h)
-            .attr("class", fretboardNutClass);
-
-    // Add the first center
-    centerPos.push(Math.floor(fret_x_coord + now_fret_size / 2));
-
-    fret_x_coord += now_fret_size + 1;
-
-    // Now, we generate all other frets
-    for (var n = 1; n < fretCount; n++) {
-
-        var now_fret_size = Math.floor(firstFretWidth * Math.exp(-0.0655 * n));
-        centerPos.push(Math.floor(fret_x_coord + now_fret_size / 2));
+        // We start with the open position (a third of the first fret size)
+        var now_fret_size = Math.floor(firstFretWidth / 3);
         fret_min_width = Math.min(fret_min_width, now_fret_size);
-
         this.svg_fretboard.append("rect").attr("x", fret_x_coord)
                 .attr("y", FRET_Y_OFFSET)
                 .attr("width", now_fret_size)
                 .attr("height", fret_h)
-                .attr("class", fretboardClass);
+                .attr("class", fretboardNutClass);
 
-        // Decide whether to draw a circle mark here
-        if (FRET_DOTS.indexOf(n % 12) !== -1)
-        {
-            var fret_dot_y = Math.floor(fret_h / 2 + FRET_Y_OFFSET);
-            var fret_dot_x = Math.floor(now_fret_size / 2 + fret_x_coord);
-            var fret_dot_r = Math.floor(FRET_DOT_DIAMETER / 2);
+        // Add the first center
+        centerPos.push(Math.floor(fret_x_coord + now_fret_size / 2));
 
-            if ((n % 12) !== 0) {
+        fret_x_coord += now_fret_size + 1;
 
-                this.svg_fretboard.append("circle").attr("cx", fret_dot_x)
-                        .attr("cy", fret_dot_y)
-                        .attr("r", fret_dot_r)
-                        .attr("class", FRET_DOT_DEFAULT_CLASS);
-            } else { // Double dot
-                this.svg_fretboard.append("circle").attr("cx", fret_dot_x)
-                        .attr("cy", Math.floor(fret_dot_y + fret_string_h))
-                        .attr("r", fret_dot_r)
-                        .attr("class", FRET_DOT_DEFAULT_CLASS);
+        // Now, we generate all other frets
+        for (var n = 1; n < fretCount; n++) {
 
-                this.svg_fretboard.append("circle").attr("cx", fret_dot_x)
-                        .attr("cy", Math.floor(fret_dot_y - fret_string_h))
-                        .attr("r", fret_dot_r)
-                        .attr("class", FRET_DOT_DEFAULT_CLASS);
+            var now_fret_size = Math.floor(firstFretWidth * Math.exp(-0.0655 * n));
+            centerPos.push(Math.floor(fret_x_coord + now_fret_size / 2));
+            fret_min_width = Math.min(fret_min_width, now_fret_size);
+
+            this.svg_fretboard.append("rect").attr("x", fret_x_coord)
+                    .attr("y", FRET_Y_OFFSET)
+                    .attr("width", now_fret_size)
+                    .attr("height", fret_h)
+                    .attr("class", fretboardClass);
+
+            // Decide whether to draw a circle mark here
+            if (FRET_DOTS.indexOf(n % 12) !== -1)
+            {
+                var fret_dot_y = Math.floor(fret_h / 2 + FRET_Y_OFFSET);
+                var fret_dot_x = Math.floor(now_fret_size / 2 + fret_x_coord);
+                var fret_dot_r = Math.floor(FRET_DOT_DIAMETER / 2);
+
+                if ((n % 12) !== 0) {
+
+                    this.svg_fretboard.append("circle").attr("cx", fret_dot_x)
+                            .attr("cy", fret_dot_y)
+                            .attr("r", fret_dot_r)
+                            .attr("class", FRET_DOT_DEFAULT_CLASS);
+                } else { // Double dot
+                    this.svg_fretboard.append("circle").attr("cx", fret_dot_x)
+                            .attr("cy", Math.floor(fret_dot_y + fret_string_h))
+                            .attr("r", fret_dot_r)
+                            .attr("class", FRET_DOT_DEFAULT_CLASS);
+
+                    this.svg_fretboard.append("circle").attr("cx", fret_dot_x)
+                            .attr("cy", Math.floor(fret_dot_y - fret_string_h))
+                            .attr("r", fret_dot_r)
+                            .attr("class", FRET_DOT_DEFAULT_CLASS);
+                }
+
             }
 
+            fret_x_coord += now_fret_size;
+
         }
 
-        fret_x_coord += now_fret_size;
+        // Compute the width of the whole fretboard
+        fret_width = fret_x_coord - FRET_X_OFFSET;
+
+        // Fret min width determines marker size...
+        var fret_marker_size = fret_min_width;
+
+        // We are now ready to draw the strings and populate the fret with notes
+        for (var i = 0; i < this.fret_notes.count; i++)
+        {
+            var string_index = i + 1;
+
+            // Compute this using a specific formula
+            var sem_distance = get_semitone_distance(this.fret_notes.full_note[i]);
+            var string_diameter = Math.round(-4.634 *
+                    Math.log(sem_distance) + 19.259);
+
+            // Clip at boundaries
+            string_diameter = Math.min(string_diameter, FRET_STRING_MAX_DIAM);
+            string_diameter = Math.max(string_diameter, FRET_STRING_MIN_DIAM);
+
+            current_string_y = FRET_Y_OFFSET + string_index * fret_string_h;
+
+            // Correct for string diameter
+            corrn = Math.floor(string_diameter / 2);
+            current_string_y -= corrn;
+
+            // First put the string shadow on there
+            current_string_shadow = this.svg_fretboard.append("line")
+                    .attr("x1", FRET_X_OFFSET)
+                    .attr("y1", current_string_y + 1)
+                    .attr("x2", FRET_X_OFFSET + fret_width)
+                    .attr("y2", current_string_y + 1)
+                    .attr("class", FRET_SHADOW_DEFAULT_CLASS)
+                    .attr("stroke-width", string_diameter);
+
+            // Now the string itself
+            current_string = this.svg_fretboard.append("line")
+                    .attr("x1", FRET_X_OFFSET)
+                    .attr("y1", current_string_y)
+                    .attr("x2", FRET_X_OFFSET + fret_width)
+                    .attr("y2", current_string_y)
+                    .attr("class", "fret-string")
+                    .attr("stroke-width", string_diameter);
+
+            // Check if this is a wound string
+            if (sem_distance < FRET_WOUND_STRING_SEM_DST)
+            {
+                current_string.classed("fret-wound-string", true);
+            }
+
+            // Populate notes
+            first_note = note_array.indexOf(this.fret_notes.note[i]);
+            first_note_index = parseInt(this.fret_notes.index[i]);
+            half_fret_marker_size = Math.floor(fret_marker_size / 2);
+            for (var j = 0; j < centerPos.length; j++)
+            {
+                now_note = note_array[(first_note + j) % note_array.length];
+                now_note_class = "note-" + now_note.replace("#", "s").toLowerCase();
+                now_note_num_class = now_note_class + first_note_index;
+                first_note_index += ((first_note + j + 1) % note_array.length === 0 ? 1 : 0);
+
+                var current_note_class = FRET_DEFAULT_MARKER_CLASS + " "
+                        + "fretboard-marker-inactive" + " "
+                        + now_note_class + " "
+                        + now_note_num_class;
+
+                fret_marker_x = centerPos[j] - half_fret_marker_size;
+                fret_marker_y = current_string_y - half_fret_marker_size;
+                var now_g = this.svg_fretboard.append("g").attr("class", current_note_class);
+                now_g.append("rect")
+                        .attr("x", fret_marker_x)
+                        .attr("y", fret_marker_y)
+                        .attr("width", fret_marker_size)
+                        .attr("height", fret_marker_size);
+                now_g.append("text")
+                        .attr("text-anchor", "middle")
+                        .attr("x", centerPos[j])
+                        .attr("y", fret_marker_y + 12)
+                        .text(now_note);
+            }
+
+            // Assign click events
+            d3.selectAll("g." + FRET_DEFAULT_MARKER_CLASS)
+                    .on('click', this.clickHandler());
+
+        }
+        
+        // Restore notes, if needed
+        this.restoreNotes();
 
     }
 
-    // Compute the width of the whole fretboard
-    fret_width = fret_x_coord - FRET_X_OFFSET;
-
-    // Fret min width determines marker size...
-    var fret_marker_size = fret_min_width;
-
-    // We are now ready to draw the strings and populate the fret with notes
-    for (var i = 0; i < this.fret_notes.count; i++)
-    {
-        var string_index = i + 1;
-
-        // Compute this using a specific formula
-        var sem_distance = get_semitone_distance(this.fret_notes.full_note[i]);
-        var string_diameter = Math.round(-4.634 *
-                Math.log(sem_distance) + 19.259);
-
-        // Clip at boundaries
-        string_diameter = Math.min(string_diameter, FRET_STRING_MAX_DIAM);
-        string_diameter = Math.max(string_diameter, FRET_STRING_MIN_DIAM);
-
-        current_string_y = FRET_Y_OFFSET + string_index * fret_string_h;
-
-        // Correct for string diameter
-        corrn = Math.floor(string_diameter / 2);
-        current_string_y -= corrn;
-
-        // First put the string shadow on there
-        current_string_shadow = this.svg_fretboard.append("line")
-                .attr("x1", FRET_X_OFFSET)
-                .attr("y1", current_string_y + 1)
-                .attr("x2", FRET_X_OFFSET + fret_width)
-                .attr("y2", current_string_y + 1)
-                .attr("class", FRET_SHADOW_DEFAULT_CLASS)
-                .attr("stroke-width", string_diameter);
-
-        // Now the string itself
-        current_string = this.svg_fretboard.append("line")
-                .attr("x1", FRET_X_OFFSET)
-                .attr("y1", current_string_y)
-                .attr("x2", FRET_X_OFFSET + fret_width)
-                .attr("y2", current_string_y)
-                .attr("class", "fret-string")
-                .attr("stroke-width", string_diameter);
-
-        // Check if this is a wound string
-        if (sem_distance < FRET_WOUND_STRING_SEM_DST)
-        {
-            current_string.classed("fret-wound-string", true);
-        }
-
-        // Populate notes
-        first_note = note_array.indexOf(this.fret_notes.note[i]);
-        first_note_index = parseInt(this.fret_notes.index[i]);
-        half_fret_marker_size = Math.floor(fret_marker_size / 2);
-        for (var j = 0; j < centerPos.length; j++)
-        {
-            now_note = note_array[(first_note + j) % note_array.length];
-            now_note_class = "note-" + now_note.replace("#", "s").toLowerCase();
-            now_note_num_class = now_note_class + first_note_index;
-            first_note_index += ((first_note + j + 1) % note_array.length === 0 ? 1 : 0);
-
-            var current_note_class = FRET_DEFAULT_MARKER_CLASS + " "
-                    + "fretboard-marker-inactive" + " "
-                    + now_note_class + " "
-                    + now_note_num_class;
-
-            fret_marker_x = centerPos[j] - half_fret_marker_size;
-            fret_marker_y = current_string_y - half_fret_marker_size;
-            var now_g = this.svg_fretboard.append("g").attr("class", current_note_class);
-            now_g.append("rect")
-                    .attr("x", fret_marker_x)
-                    .attr("y", fret_marker_y)
-                    .attr("width", fret_marker_size)
-                    .attr("height", fret_marker_size);
-            now_g.append("text")
-                    .attr("text-anchor", "middle")
-                    .attr("x", centerPos[j])
-                    .attr("y", fret_marker_y + 12)
-                    .text(now_note);
-        }
-
-        // Assign click events
-        d3.selectAll("g." + FRET_DEFAULT_MARKER_CLASS)
-                .on('click', this.clickHandler());
-
-    }
 
     // Draw particular note, e.g., "F#4" or clear it via display = false
     this.updateSingleNote = function (note, display) {
@@ -1499,6 +1521,13 @@ function comptoolsFretboard(cont_class, tuning, options) {
         this.svg_fretboard.selectAll(".note-" + note.replace("#", "s").
                 toLowerCase()).classed("fretboard-marker-selected", display);
 
+    };
+
+    // TODO: Restore notes
+    this.restoreNotes = function(){
+        if (this.notes !== null){
+            // Do some magic here
+        }
     };
 
     // Draw based on note sets, e.g., ["A", "B", "C", "D", "E", "F#", "G"]
@@ -1551,7 +1580,7 @@ function comptoolsFretboard(cont_class, tuning, options) {
                 .classed("fretboard-marker-highlighted-root", false);
     };
 
-    this.accidental_state = "sharps"; // TODO: Allowed values: sharps/flats
+    this.accidental_state = "sharps";
 
     this.sharps2flats = function () {
         if (this.accidental_state === "sharps") {
@@ -1577,6 +1606,9 @@ function comptoolsFretboard(cont_class, tuning, options) {
         }
         this.accidental_state = "sharps";
     };
+
+    // Update the fretboard (initializtion)
+    this.updateTuning();
 
 }
 
